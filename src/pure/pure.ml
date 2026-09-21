005173f2295665ea76552f1e4f958062a6b57de5
Abdelghani ALIDRA <alidrandco@yahoo.fr>
2026-09-21 12:58:56 +0200

    add position to exception

--- src/pure/pure.ml
+++ src/pure/pure.ml
@@ -76,9 +76,6 @@ let parse_command p : (Command.t, Pos.popt * string) Result.t =
 
 (** Exception raised by [parse_text] on error. *)
 
-let prefix_modified (m : Syntax.p_ident) :  Syntax.p_ident =
-            {m with elt = "CHANGEE: " ^ m.elt}
-
 let parse_text :
       fname:string -> string -> Command.t list * (Pos.pos * string) option =
   fun ~fname s ->
@@ -93,21 +90,14 @@ let parse_text :
     Stream.iter (fun c -> Stdlib.(cmds := c :: !cmds)) (parse_string fname s);
     List.rev Stdlib.(!cmds), None
   with
-  | LpLexer.UnfinishedProof (m, s) ->
-    if true then
-    (* if String.starts_with ~prefix:"MODIFIED" m.elt then *)
-      begin
-        let m = prefix_modified m in
-        let cmd = Syntax.P_symbol s in
-        let cmd = {Pos.elt=cmd;Pos.pos=s.p_sym_kw} in
-        Stdlib.(cmds := cmd :: !cmds);
-        let loc = match m.pos with
-        | Some pos -> pos | None -> assert false in
-        List.rev Stdlib.(!cmds), Some(loc, m.elt)
-      end
-    else
-      assert false
-
+  | LpLexer.UnfinishedProof (m, s,pos) ->
+    begin
+      let cmd = {Pos.elt=Syntax.P_symbol s;Pos.pos=pos} in
+      Stdlib.(cmds := cmd :: !cmds);
+      let loc = match m.pos with
+      | Some pos -> pos | None -> assert false in
+      List.rev Stdlib.(!cmds), Some(loc, m.elt)
+    end
   | Fatal(Some(Some(pos)), msg, err_desc) ->
       List.rev Stdlib.(!cmds), Some(pos, msg ^ "\n" ^ err_desc)
   | Fatal(Some(None)     , _  , _) -> assert false
@@ -126,20 +116,14 @@ let parse_file :
     Stream.iter (fun c -> Stdlib.(cmds := c :: !cmds)) (parse_file fname);
     List.rev Stdlib.(!cmds), None
   with
-  | LpLexer.UnfinishedProof (m, s) ->
-    if true then
-    (* if String.starts_with ~prefix:"MODIFIED" m.elt then *)
-      begin
-        let m = prefix_modified m in
-        let cmd = Syntax.P_symbol s in
-        let cmd = {Pos.elt=cmd;Pos.pos=s.p_sym_kw} in
-        Stdlib.(cmds := cmd :: !cmds);
-        let loc = match m.pos with
-        | Some pos -> pos | None -> assert false in
-        List.rev Stdlib.(!cmds), Some(loc, m.elt)
-      end
-    else
-      assert false
+  | LpLexer.UnfinishedProof (m, s, pos) ->
+    begin
+      let cmd = {Pos.elt=Syntax.P_symbol s;Pos.pos=pos} in
+      Stdlib.(cmds := cmd :: !cmds);
+      let loc = match m.pos with
+      | Some pos -> pos | None -> assert false in
+      List.rev Stdlib.(!cmds), Some(loc, m.elt)
+    end
   | Fatal(Some(Some(pos)), msg, desc) ->
       List.rev Stdlib.(!cmds), Some(pos, msg ^ ". " ^ desc)
   | Fatal(Some(None)     , _ , _ ) -> assert false
